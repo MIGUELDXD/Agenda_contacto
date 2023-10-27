2233d8eda8cca628d405778986b2abb4a2bc314f
MIGUELDXD <145181768+MIGUELDXD@users.noreply.github.com>
2023-10-26 21:15:36 -0600

    Add files via upload

---
+++ AGENDA_CONTACTOS/src/Formularios/conexionMysql.java
@@ -0,0 +1,45 @@
+package Formularios;
+
+import java.sql.Connection; //se modifica com.mysql.jdbc.Connection
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Eduardo Umul
+ */
+public class conexionMysql {
+   static String bd = "agenda_contactos";
+   static String login = "root";
+   static String password = "";
+   static String url = "jdbc:mysql://localhost/"+bd;
+
+   Connection conn = null;
+
+   /** Constructor de DbConnection */
+   public conexionMysql () {
+      try{
+         //obtenemos el driver de para mysql
+         Class.forName("com.mysql.jdbc.Driver");
+         //obtenemos la conexión
+         conn = DriverManager.getConnection(url,login,password);
+         if (conn!=null){
+            System.out.println("Conección a base de datos "+bd+". listo");
+         }
+      }catch(SQLException e){
+         System.out.println(e);
+      }catch(ClassNotFoundException e){
+         System.out.println(e);
+      }
+   }
+   /**Permite retornar la conexión*/
+   public Connection getConnection(){
+      return conn;
+   }
+
+   public void desconectar(){
+      conn = null;
+      System.out.println("La conexion a la  base de datos "+bd+" a terminado");
+   }
+}
+
